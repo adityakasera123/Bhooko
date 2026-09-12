66974d49e14b9ed892a5613dc1379cc4406a5dbc
Aditya <adityakasera40@gmail.com>
2026-09-12 15:41:01 +0530

    feat(backend): add environment configuration

--- backend/api/src/app.module.ts
+++ backend/api/src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HealthModule } from './health/health.module';
 
 @Module({
-  imports: [HealthModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    HealthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
